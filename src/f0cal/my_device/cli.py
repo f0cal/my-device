e30dc713f75ec9789a322c89cf35fa7163133294
quincy2112 <qphiwum@gmail.com>
2020-10-20 19:34:59 -0400

    Update device-farm, my-device

--- src/f0cal/my_device/cli.py
+++ src/f0cal/my_device/cli.py
@@ -1,7 +1,7 @@
 # import f0cal
 # from .conan_utils.image import Image
 #
-# @f0cal.entrypoint(['my-device', 'image', 'install'], args=_image_install_args)
+# @f0cal.CORE.entrypoint(['my-device', 'image', 'install'], args=_image_install_args)
 # def _image_install(parser, core, image, device_type):
 #     ref = ConanFileReference.loads(image, validate=False)
 #     info = Conan().install_reference(ref, generators=["f0cal"])
