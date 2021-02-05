86c6e8523610d1de5fb17ac79f4a901a0f78ef6d
quincy2112 <qphiwum@gmail.com>
2021-02-04 23:40:20 -0500

    Change imports back to "import f0cal.core"
    Fix import in farm client generated code

--- src/f0cal/my_device/cli.py
+++ src/f0cal/my_device/cli.py
@@ -1,7 +1,7 @@
-# from f0cal import core
+# import f0cal.core
 # from .conan_utils.image import Image
 #
-# @f0cal.CORE.entrypoint(['my-device', 'image', 'install'], args=_image_install_args)
+# @f0cal.core.CORE.entrypoint(['my-device', 'image', 'install'], args=_image_install_args)
 # def _image_install(parser, core, image, device_type):
 #     ref = ConanFileReference.loads(image, validate=False)
 #     info = Conan().install_reference(ref, generators=["f0cal"])
